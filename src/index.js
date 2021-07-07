9566ec3b2a11958215ab55e8fab8eb6678fd8bd7
keiryquero <keiryquero@gmail.com>
2021-07-07 13:27:14 -0500

    maquetacion y practica de las primeras funciones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import validator from './validator.js';
+//import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+function validar() {
+  //1. recibe el numero
+  let numero = document.getElementById("tarjeta").value;
+  console.log(numero);
+  console.log(typeof numero);
+   if (numero.length >= 13) {
+    if (numero.length <= 16) {
+      console.log('el numero es correcto');
+    } else {
+      console.log('el numero es incorrecto');
+    }
+  } else {
+    console.log('el numero es incorrecto');
+  }
+}
+  
+function botonVista() {
+  console.log("Funcion Boton Vista");
+  //1. Ocultar primera Vista
+  document.getElementById("primeraVista").style.display = "none";
+  //2. Mostrar Mi segunda Vista
+  document.getElementById("segundaVista").style.display = "block";
+
+}
