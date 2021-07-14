f57fb1dab8ef63c8267c12c873e4485f35920497
keiryquero <keiryquero@gmail.com>
2021-07-14 00:13:59 -0500

    actualizacion masiva de todos los archivos parte 1

--- src/index.js
+++ src/index.js
@@ -1,24 +1,19 @@
-//import validator from './validator.js';
+import validator from './validator.js';
+//input del numero de la tarjeta
+let input = document.getElementById("inputNumero");
+/*input.addEventListener('keyup', () => {
+  //la etiqueta cambia a valor
+     let valorInput =input.value;
+     //console.log(typeof valorInput);
+     validator.isValid(valorInput);
+     
+      });*/
+   //invoca la funcion del boton validar   
+document.getElementById("validar").addEventListener("click", () => { validator.isValid(input.value) })
+// invoca la funcion del boton comprar
+document.getElementById("botonComprar").addEventListener("click", () => { comprar() })
 
-//console.log(validator);
-function validar() {
-  //1. recibe el numero
-  let numero = document.getElementById("tarjeta").value;
-  console.log(numero);
-  console.log(typeof numero);
-   if (numero.length >= 13) {
-    if (numero.length <= 16) {
-      console.log('el numero es correcto');
-    } else {
-      console.log('el numero es incorrecto');
-    }
-  } else {
-    console.log('el numero es incorrecto');
-  }
-}
-  
-function botonVista() {
-  console.log("Funcion Boton Vista");
+function comprar() {
   //1. Ocultar primera Vista
   document.getElementById("primeraVista").style.display = "none";
   //2. Mostrar Mi segunda Vista
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,29 @@
-const validator = {
-  // ...
-};
+const validator =
+{
+  isValid: function (creditCardNumber) {
+    //pasa un satring a un array
+    let nunCard = creditCardNumber.split("");
+    //revertir los numeros del array
+    let nunCardreversed = nunCard.reverse();
+    let newArray = [];
+    // ciclo para recorrer un array
+    for (var i = 0; i < nunCardreversed.length; i++) {
+      newArray.push(nunCardreversed[i]);
+      // permite validar la posicion impar
+      if (i % 2 == 1) {
+        nunCardreversed[i] = nunCardreversed[i] * 2
+        var nuevoNum = nunCardreversed[i];
+        if(nuevoNum >= 10){
+          //nuevoNum = nuevoNum.toString();
+          //nuevoNum.split("", "");
+        console.log(nuevoNum);
+        }
+        
+      }
+
+      console.log(newArray);
+    }
+  }
+}
 
 export default validator;
